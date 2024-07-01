26b5451aac0758be1b04b572197f08f16b7f8d0f
Joan Marti <joanmartimarsal@gmail.com>
2024-07-01 13:47:49 +0200

    Js con los datos de SQL

---
+++ jsonFalsificado.js
@@ -0,0 +1,32 @@
+const articles = [
+    {
+        ID: 1,
+        Titular: "Borjita",
+        Contenido: "Borja usó Gruñido"
+    },
+    {
+        ID: 2,
+        Titular: "Maikol",
+        Contenido: "Maikol usó Doble Monster"
+    },
+    {
+        ID: 3,
+        Titular: "Jose Luis Torrente",
+        Contenido: "Torrente sacó las pistolitas"
+    },
+    {
+        ID: 4,
+        Titular: "Antonio",
+        Contenido: "Antonio se durmió en la obra"
+    },
+    {
+        ID: 5,
+        Titular: "Mbappe",
+        Contenido: "Mbappe no sabe sacar al ping ping"
+    }
+];
+
+
+module.exports = articles;
+
+console.log(articles);
